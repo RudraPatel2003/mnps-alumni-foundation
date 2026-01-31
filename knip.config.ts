95e1d22ee3bd9ee03c3bacbbc9ced3edee91d8f5
Rudra Patel <patelrudra2003@gmail.com>
2026-01-31 18:17:23 -0500

    feat: Set up project

---
+++ knip.config.ts
@@ -0,0 +1,8 @@
+import type { KnipConfig } from "knip";
+
+const config: KnipConfig = {
+  // needed for eslint to work properly
+  ignoreDependencies: ["@typescript-eslint/parser"],
+};
+
+export default config;
